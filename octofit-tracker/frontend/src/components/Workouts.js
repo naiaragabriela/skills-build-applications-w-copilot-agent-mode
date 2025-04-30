7c7266c7f7f844b6cb9ba33c9d978fb71dcc77d3
Naiara Gabriela <126898837+naiaragabriela@users.noreply.github.com>
2025-04-30 18:06:15 +0000

    feat: Add frontend

---
+++ octofit-tracker/frontend/src/components/Workouts.js
@@ -0,0 +1,39 @@
+import React, { useEffect, useState } from 'react';
+
+function Workouts() {
+  const [workouts, setWorkouts] = useState([]);
+
+  useEffect(() => {
+    fetch('https://expert-disco-x6j4vpxwv7jhqrg-8000.app.github.dev/api/workouts/')
+      .then(res => res.json())
+      .then(data => setWorkouts(data));
+  }, []);
+
+  return (
+    <div className="card mt-4">
+      <div className="card-body">
+        <h2 className="card-title mb-4">Workouts</h2>
+        <div className="table-responsive">
+          <table className="table table-striped table-hover">
+            <thead className="table-dark">
+              <tr>
+                <th>Name</th>
+                <th>Description</th>
+              </tr>
+            </thead>
+            <tbody>
+              {workouts.map(workout => (
+                <tr key={workout._id}>
+                  <td>{workout.name}</td>
+                  <td>{workout.description}</td>
+                </tr>
+              ))}
+            </tbody>
+          </table>
+        </div>
+      </div>
+    </div>
+  );
+}
+
+export default Workouts;
